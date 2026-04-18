f060041c6a63190a72f55ca3fb01bed06c6c3fce
David-Toas <showoleolabode.6@gmail.com>
2026-04-18 13:38:08 +0100

    Add initial HTML and JavaScript files for project setup

---
+++ Javascript/index.js
@@ -0,0 +1,51 @@
+// alert is used to show a pop-up message to the user
+// alert("welcome to javascript");
+// console.log is used to print a message to the console
+console.log("welcome to javascript");
+
+// Variable declaration
+// string
+const name = "Olabode";
+// console.log(typeof name);
+// console.log(name);
+
+// number
+let age = 19;
+// console.log(typeof age);
+// console.log(age);
+
+// boolean
+let walking = false;
+// console.log(walking);
+// console.log(typeof walking);
+
+let addition = 5 + 10;
+// console.log(addition);
+
+let addMe = 5 + "10";
+// console.log(addMe);
+
+// undefined is used to represent a variable that has not been assigned a value
+let a;
+// console.log(a);
+// console.log(typeof a);
+
+// null is used to represent the absence of a value
+let b = null;
+// console.log(b);
+// console.log(typeof b);
+
+// -------------------------------------------------------------------------
+// Operators
+// ---------------------------------------------
+// Arithmetic operators
+// + - * / % **
+
+let x = 10;
+let y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
